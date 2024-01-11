39274d76066401e58ce2da9c86a6a1654b42ca2f
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-01-11 10:46:20 -0600

    implemented dynammic content

--- nodeServer/routes/shop.js
+++ nodeServer/routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router(); //mini express app that we can export
 
 router.get("/", (req, res, next) => { //if it starts with /, it will execute this middleware. other routes must be on top
     const products = adminData.products;
-    res.render("shop", {prods: products, docTitle: "Shop"});
+    res.render("shop", {prods: products, docTitle: "Shop", path: "/"});
 });
 
 
