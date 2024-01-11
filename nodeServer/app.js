3434a0fe4ce010c14b097871bf7b7bf728905f05
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-01-11 10:30:40 -0600

    Created pug file for all views

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -36,8 +36,9 @@ app.use("/admin", adminData.routes); // this will register the adminRoutes middl
 app.use(shopRoutes);
 
 app.use("/", (req, res, next) => { //if it starts with /, it will execute this middleware. other routes must be on top
-    console.log('In the next middleware');
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    //console.log('In the next middleware');
+    //res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).render("404", {docTitle: "Page Not Found"});
 });
 
 
