a46dcf48e08a2244dab27ac9862c5c507cbdee22
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-01-12 20:31:07 -0600

    refactored file storage code

--- nodeServer/models/product.js
+++ nodeServer/models/product.js
@@ -2,45 +2,41 @@ const fs = require("fs");
 const path = require("path");
 const rootDir = require("../util/path");
 
-module.exports = class Product {
-  constructor(title) {
-    this.title = title;
-  }
+const p = path.join(rootDir, "data", "products.json");
 
-  save() {
-    //path to saving products in a file
-    const p = path.join(rootDir, "data", "products.json");
 
-    //read the file
-    fs.readFile(p, (err, fileContent) => {
-      // this is a callback function that will be executed once the file is read
-      let products = [];
-
-      if (!err) {
-        //if there is an error, return an empty array
-        products = JSON.parse(fileContent); // this will parse the file content into a javascript object
-      }
-
-      products.push(this); // this refers to the object created by the constructor
-
-      //write the file
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
-    });
-  }
-
-  static fetchAll(cb) {
-
-    const p = path.join(rootDir, "data", "products.json");
+const getProductsFromFile = (cb) => {
 
     fs.readFile(p, (err, fileContent) => {
       // this is a callback function that will be executed once the file is read
       if (err) {
         cb([]);; //if there is an error, return an empty array
       }
-      cb(JSON.parse(fileContent)); // this will parse the file content into a javascript object
+      else{
+        cb(JSON.parse(fileContent)); // this will parse the file content into a javascript object
+      }
     });
     //return products;
   }
+
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+        products.push(this); // this refers to the object created by the constructor
+
+        //write the file
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  };
 };
