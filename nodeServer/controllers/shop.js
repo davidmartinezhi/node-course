7658b068f6605d60325297589f185363384e05ef
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-01-18 10:41:40 -0600

    imported sequelize

--- nodeServer/controllers/shop.js
+++ nodeServer/controllers/shop.js
@@ -119,7 +119,6 @@ exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
     .then(([product]) => {
-      console.log(product);
       res.render("shop/product-detail", {
         product: product[0],
         docTitle: product[0].title,
