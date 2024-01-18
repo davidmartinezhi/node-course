1eccae9af5fc91af64686445857a5f0c22310551
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-01-18 16:28:07 -0600

    Fetching admin products

--- nodeServer/controllers/admin.js
+++ nodeServer/controllers/admin.js
@@ -33,7 +33,7 @@ exports.postAddProduct = (req, res, next) => {
     description: description,
     price: price,
   })
-    .then( result => {
+    .then((result) => {
       //console.log(result);
       console.log("Created Product");
     })
@@ -94,11 +94,22 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("admin/products", {
-      prods: products,
-      docTitle: "Admin Products",
-      path: "/admin/products",
-    });
-  }); // this will fetch all the products
+  Product.findAll()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        docTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => console.log(err));
+
+  //just sql
+  // Product.fetchAll((products) => {
+  //   res.render("admin/products", {
+  //     prods: products,
+  //     docTitle: "Admin Products",
+  //     path: "/admin/products",
+  //   });
+  // }); // this will fetch all the products
 };
