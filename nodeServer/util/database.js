246245efa17ab7ad4d84a274c7921d618969c958
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-01-18 10:48:08 -0600

    Connecting to the db

--- nodeServer/util/database.js
+++ nodeServer/util/database.js
@@ -1,15 +1,27 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const Sequelize = require("sequelize");
 
 const password = process.env.PASSWORD_MYSQL;
 
-// create a pool of connections 
-const pool = mysql.createPool({
-    host: "localhost", 
-    user: "root",
-    database: "node-complete",
-    password: password
+const sequelize = new Sequelize("node-complete", "root", password, {
+  dialect: "mysql",
+  host: "localhost",
 });
 
-// promises are better than callbacks
-module.exports = pool.promise(); // this will export a promise
+module.exports = sequelize;
+
+//Sequelize does this behind the scenes
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const password = process.env.PASSWORD_MYSQL;
+
+// // create a pool of connections
+// const pool = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     database: "node-complete",
+//     password: password
+// });
+
+// // promises are better than callbacks
+// module.exports = pool.promise(); // this will export a promise
