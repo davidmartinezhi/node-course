2b8cbb62b127dbf2ed90a4a5989ab8dd843f949b
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-03-26 22:03:57 -0600

    added validation checks to admin routes

--- nodeServer/routes/admin.js
+++ nodeServer/routes/admin.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-
+const { check } = require("express-validator");
 //const rootDir = require("../util/path"); // this will give us the path to the root directory of our project
 
 const adminController = require("../controllers/admin");
@@ -8,23 +8,42 @@ const isAuth = require("../middleware/is-auth"); // we get middleware to validat
 
 const router = express.Router(); //mini express app that we can export
 
-
 // /admin/add-product => GET
 //on the router we register things like get, post, use, etc
-router.get("/add-product", isAuth, adminController.getAddProduct); 
+router.get("/add-product", isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
 router.get("/products", isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
 //app.get() is a method that allows us to register a new middleware function that will only be executed for incoming GET requests
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post(
+  "/add-product",
+  [
+    check("title").isString().isLength({ min: 3 }).trim(),
+    check("imageUrl").isURL(),
+    check("price").isNumeric(),
+    check("description").isLength({ min: 5, max: 400 }),
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
 
 // /admin/edit-product => GET
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
 // /admin/edit-product => POST
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+  "/edit-product",
+  [
+    check("title").isString().isLength({ min: 3 }).trim(),
+    check("imageUrl").isURL(),
+    check("price").isNumeric(),
+    check("description").isLength({ min: 5, max: 400 }),
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
 
 // /admin/delete-product => POST
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
