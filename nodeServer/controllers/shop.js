278f5d9547d9c8d91ed2a57985e427c5d1144c35
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-04-01 12:42:11 -0600

    added functionality to stream data

--- nodeServer/controllers/shop.js
+++ nodeServer/controllers/shop.js
@@ -18,15 +18,14 @@ exports.getProducts = (req, res, next) => {
         pageTitle: "All Products",
         path: "/products",
         isAuthenticated: req.session.isLoggedIn,
-
       });
     })
     .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
-  
+
       // this will skip all the other middlewares ang go to the error handling middleware
-      return next(error); 
+      return next(error);
     });
 };
 
@@ -49,9 +48,9 @@ exports.getIndex = (req, res, next) => {
     .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
-  
+
       // this will skip all the other middlewares ang go to the error handling middleware
-      return next(error); 
+      return next(error);
     });
 
   // this will fetch all the products with just sql
@@ -83,14 +82,13 @@ exports.getCart = async (req, res, next) => {
       path: "/cart",
       products: products,
       isAuthenticated: req.session.isLoggedIn,
-
     });
   } catch (err) {
     const error = new Error(err);
     error.httpStatusCode = 500;
 
     // this will skip all the other middlewares ang go to the error handling middleware
-    return next(error); 
+    return next(error);
   }
 };
 
@@ -107,7 +105,7 @@ exports.postCart = async (req, res, next) => {
     error.httpStatusCode = 500;
 
     // this will skip all the other middlewares ang go to the error handling middleware
-    return next(error); 
+    return next(error);
   }
 };
 
@@ -122,9 +120,9 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
-  
+
       // this will skip all the other middlewares ang go to the error handling middleware
-      return next(error); 
+      return next(error);
     });
   // Product.findById(prodId, (product) => {
   //   Cart.deleteProduct(prodId, +product.price); // this will delete the product from the cart
@@ -161,7 +159,7 @@ exports.postOrder = async (req, res, next) => {
     error.httpStatusCode = 500;
 
     // this will skip all the other middlewares ang go to the error handling middleware
-    return next(error); 
+    return next(error);
   }
 };
 
@@ -173,7 +171,6 @@ exports.postOrder = async (req, res, next) => {
  */
 exports.getOrders = async (req, res, next) => {
   try {
-
     const orders = await req.user.getOrders();
 
     res.render("shop/orders", {
@@ -187,7 +184,7 @@ exports.getOrders = async (req, res, next) => {
     error.httpStatusCode = 500;
 
     // this will skip all the other middlewares ang go to the error handling middleware
-    return next(error); 
+    return next(error);
   }
 };
 
@@ -220,9 +217,9 @@ exports.getProduct = (req, res, next) => {
     .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
-  
+
       // this will skip all the other middlewares ang go to the error handling middleware
-      return next(error); 
+      return next(error);
     });
 };
 
@@ -232,31 +229,58 @@ exports.getInvoice = (req, res, next) => {
   const invoiceName = "invoice-" + orderId + ".pdf"; // this will store the invoice name
   const invoicePath = path.join("data", "invoices", invoiceName); // this will store the invoice path
 
-  fs.readFile(invoicePath, (err, data) => {
-    if (err) {
-      return next(err); // this will skip all the other middlewares ang go to the error handling middleware
-    }
+  console.log("order id: ", orderId);
+  console.log("invoice name: ", invoiceName);
+  console.log("invoice path: ", invoicePath);
 
-    //we validate if the order exists and if the user is the owner of the order
-    Order.findById({_id: orderId}).then(order => {
-
+  //we validate if the order exists and if the user is the owner of the order
+  Order.findById({ _id: orderId })
+    .then((order) => {
       //validate if we have an order
-      if(!order){
-        return next(new Error('No order found'))
+      if (!order) {
+        console.log("No order found");
+        return next(new Error("No order found"));
       }
 
       //validate if the user is the owner of the order
-      if(order.user.userId.toString() !== req.user._id.toString()){
-        return next(new Error('Unauthorized'))
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        console.log("Unauthorized");
+        return next(new Error("Unauthorized"));
       }
-    }).catch(err => {
-      return next(err)
-    });
 
-    //if we dont have and error, we send the file
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", 'inline; filename="' + invoiceName + '"'); // this will open the pdf in the browser
-    // res.setHeader("Content-Disposition", 'attachment; filename="' + invoiceName + '"'); // this will force the download
-    res.send(data);
-  });
+      //if we have an order and the user is the owner, we send the file
+      //for big files, we can use streams.
+      //streaming is better because we dont have to load the entire file into memory
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) {
+      //     console.log(err);
+      //     return next(err); // this will skip all the other middlewares ang go to the error handling middleware
+      //   }
+      //   //if we dont have and error, we send the file
+      //   res.setHeader("Content-Type", "application/pdf");
+      //   res.setHeader(
+      //     "Content-Disposition",
+      //     'inline; filename="' + invoiceName + '"'
+      //   ); // this will open the pdf in the browser
+      //   // res.setHeader("Content-Disposition", 'attachment; filename="' + invoiceName + '"'); // this will force the download
+      //   res.send(data);
+      // });
+
+      //streaming the file
+      const file = fs.createReadStream(invoicePath); // this will create a read stream
+
+      //we set the headers
+      res.setHeader("Content-Type", "application/pdf");
+
+      //we pipe the file to the response
+      //for large files this is a huge advantage
+      //node never has to preload the entire file into memory
+      //it just streams it to the client on the flight
+      //the most it has to store is one chunk of data, we foward them to the browser and it concatenates the chunks
+      file.pipe(res); //readable stream can be piped to writeable stream(res) and vice versa 
+
+    })
+    .catch((err) => {
+      return next(err);
+    });
 };
