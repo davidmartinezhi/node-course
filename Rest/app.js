a8a39b8295bbc3c4b2463e2e957e2156f089eb8a
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-04-10 11:33:49 -0600

    Sended request and responses

--- Rest/app.js
+++ Rest/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express(); // this will create an express application
 
+//routes
 const feedRoutes = require("./routes/feed");
+
+//middleware
+//app.user(bodyParser.urlencoded({ extended: false })); // this will parse the body of the incoming request from forms in format x-www-form-urlencoded
+app.use(bodyParser.json()); // this will parse the body of the incoming request
 
 app.use('/feed', feedRoutes); // this will register the feedRoutes
 
--- Rest/controllers/feed.js
+++ Rest/controllers/feed.js
@@ -3,8 +3,19 @@
 module.exports = class ControllerFeed{
 
     static getPosts = (req, res, next) => {
+        // we return a json object with a posts array that contains an object with a title and content
         res.status(200).json({
             posts: [{ title: "First Post", content: "This is the first post!" }],
         });
-    }
+    };
+
+    static createPost = (req, res, next) => {
+        const title = req.body.title;
+        const content = req.body.content;
+        // Create post in db
+        res.status(201).json({ // 201 is the status code for created
+            message: "Post created successfully!",
+            post: { id: new Date().toISOString(), title: title, content: content },
+        });
+    };
 }
