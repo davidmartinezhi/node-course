e29a146d34ff30539ed7e9e2f05acf12051fa376
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-04-11 10:24:26 -0600

    Allowed cors access

--- Rest/app.js
+++ Rest/app.js
@@ -9,6 +9,14 @@ const feedRoutes = require("./routes/feed");
 //middleware
 //app.user(bodyParser.urlencoded({ extended: false })); // this will parse the body of the incoming request from forms in format x-www-form-urlencoded
 app.use(bodyParser.json()); // this will parse the body of the incoming request
+app.use((req, res, next) => {
+    //every response will have these headers
+    //I can lock this dow to my specific frontend domain. multiple domains can be separated by commas
+    res.setHeader("Access-Control-Allow-Origin", "*"); // this will allow access to any client from the server
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE"); // this will allow the methods that can be used
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // this will allow the headers that can be used
+    next();
+});
 
 app.use('/feed', feedRoutes); // this will register the feedRoutes
 
