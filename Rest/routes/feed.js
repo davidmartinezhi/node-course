969c3cd27c9773b8cb7c95a34ac345a8925db05c
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-04-11 11:24:16 -0600

    adding a create post endpoint

--- Rest/routes/feed.js
+++ Rest/routes/feed.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const feedController = require('../controllers/feed');
 
+//GET /feed/posts
 router.get('/posts', feedController.getPosts);
+
+//POST /feed/post
 router.post('/post', feedController.createPost);
 
 module.exports = router;
