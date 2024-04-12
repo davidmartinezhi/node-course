c2b1086530664638053f814eaad731e54852c5f0
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-04-12 11:02:23 -0600

    set up of post model

--- Rest/app.js
+++ Rest/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express(); // this will create an express application
 
@@ -20,4 +21,11 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes); // this will register the feedRoutes
 
-app.listen(8080); // this will start a server on port 3000
+mongoose.connect(
+    "mongodb+srv://david:dZiATPxy4lpvAc0e@cluster0.7pba9hx.mongodb.net/messages?retryWrites=true&w=majority", // this will connect to the database
+    { useNewUrlParser: true, useUnifiedTopology: true }
+).then(result => {
+    app.listen(8080); // this will start a server on port 3000
+}).catch(err => {
+    console.log(err);
+});
---
+++ Rest/models/post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: Object,
+      required: true,
+    },
+  },
+  { timestamps: true } // this will add a createdAt and updatedAt field to the schema
+);
+
+exports.Post = mongoose.model("Post", postSchema); // this will create a model called Post with the schema postSchema
