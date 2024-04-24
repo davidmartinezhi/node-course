45ffdecfef4fdfa9d9d0fe729f2433c99d27ce2f
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-04-24 10:51:07 -0600

    defined mutation schema

--- Graph/graphql/schema.js
+++ Graph/graphql/schema.js
@@ -1,16 +1,44 @@
 const { buildSchema } = require('graphql'); //build a schema that could be parsed by graphql
 
+// input type is used to define the input types for the mutation
+// ID is a special type in graphql that is used to define the unique identifier for the object
+
+// Query -> GET
+// Mutation -> POST, PUT, PATCH, Update
+// Subscription -> set up real time connections via web sockets
+
 module.exports = buildSchema(`
-    type TestData {
-        text: String!
-        views: Int!
+
+    type Post{
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
     }
 
-    type RootQuery {
-        hello: TestData!
+    type User{
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
+    }
+
+    input UserInput{
+        email: String!
+        name: String!
+        password: String!
+    }
+
+    type RootMutation{
+        createUser(userInput: UserInput): User!
     }
 
     schema {
-        query: RootQuery
+        mutation: RootMutation
     }
 `);
