310906df797b367abd5c1c12ea065bd9ce7320e9
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-04-30 09:45:54 -0600

    updated code for graphwql query to get posts

--- 00-frontend-starting-setup/src/pages/Feed/Feed.js
+++ 00-frontend-starting-setup/src/pages/Feed/Feed.js
@@ -218,20 +218,21 @@ class Feed extends Component {
           createdAt: resData.data.createPost.createdAt,
         };
         this.setState((prevState) => {
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              (p) => p._id === prevState.editPost._id
+            );
+            updatedPosts[postIndex] = post;
+          } else {
+            updatedPosts.unshift(post);
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
           };
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        this.setState({
-          isEditing: false,
-          editPost: null,
-          editLoading: false,
-          error: err,
         });
       });
   };
