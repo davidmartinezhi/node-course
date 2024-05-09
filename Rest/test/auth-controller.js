db85f522488930ac12b9ec48481aa78644dc4b16
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-05-09 12:28:53 -0600

    testing code with an active database

--- Rest/test/auth-controller.js
+++ Rest/test/auth-controller.js
@@ -1,11 +1,12 @@
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const mongoose = require("mongoose");
 
 const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
 describe("Auth Controller - Login", function () {
-  it("should throw an error with status code 500 if accessing the database fails", (done) => {
+  it("Should throw an error with status code 500 if accessing the database fails", (done) => {
     const req = {
       body: {
         email: "test@test.com",
@@ -28,5 +29,47 @@ describe("Auth Controller - Login", function () {
         findOneStub.restore();
       }
     });
+  });
+
+  it("Should send a response with a valid user status for an existing user", (done) => {
+    mongoose
+      .connect(
+        "mongodb+srv://david:dZiATPxy4lpvAc0e@cluster0.7pba9hx.mongodb.net/test-messages?retryWrites=true&w=majority", // this will connect to the database
+        { useNewUrlParser: true, useUnifiedTopology: true }
+      )
+      .then((result) => {
+        const user = new User({
+          email: "test@test.com",
+          password: "password",
+          name: "test",
+          posts: [],
+          _id: "5c0f66b979af55031b34728a",
+        });
+
+        return user.save();
+      })
+      .then(() => {
+        req = { userId: "5c0f66b979af55031b34728a" };
+        res = {
+          statusCode: 500,
+          userStatus: null,
+          status: function (code) {
+            this.statusCode = code;
+            return this;
+          },
+          json: function (data) {
+            this.userStatus = data.status;
+          },
+        };
+
+        AuthController.getUserStatus(req, res, () => {}).then(() => {
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.userStatus).to.be.equal("I am new!");
+          User.deleteMany({}).then(() => {
+            return mongoose.disconnect().then(() => done());
+          });
+        });
+      })
+      .catch((err) => done(err));
   });
 });
