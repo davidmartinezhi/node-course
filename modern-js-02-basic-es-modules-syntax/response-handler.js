3e45b2adb3c1625d491f15e1d24bcd74c720761d
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-05-14 14:47:22 -0600

    added node core modules promises

--- modern-js-02-basic-es-modules-syntax/response-handler.js
+++ modern-js-02-basic-es-modules-syntax/response-handler.js
@@ -1,17 +1,22 @@
-// const fs = require('fs');
-import fs from 'fs';
+// const fs = require('fs').promises;
+import fs from "fs/promises";
 
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const resHandler = (req, res, next) => {
-  // fs.readFile('my-page.html', 'utf8', (err, data) => {
-  //   res.send(data);
-  // });
-  res.sendFile(path.join(__dirname, 'my-page.html'));
+  fs.readFile("my-page.html", "utf8")
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("<h1>Something went wrong</h1>");
+    });
+  // res.sendFile(path.join(__dirname, 'my-page.html'));
 };
 
 // module.exports = resHandler;
