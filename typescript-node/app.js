7468c1cdc1f24c9f29140f024c5b1190d67f6daf
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-05-15 10:17:18 -0600

    added types for express and configured tsconfig

--- typescript-node/app.js
+++ typescript-node/app.js
@@ -1,4 +1,15 @@
-const express = require("express");
-const app = express();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+/*
+npm install --save-dev @types/node
+npm install --save-dev @types/express
 
-app.listen(3000);
+we will find many @types packages because they do
+translations from JavaScript to TypeScript, so TypeScript can understand them.
+*/
+app.listen({ port: 3000 });
---
+++ typescript-node/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+/*
+npm install --save-dev @types/node
+npm install --save-dev @types/express
+
+we will find many @types packages because they do
+translations from JavaScript to TypeScript, so TypeScript can understand them.
+*/
+
+app.listen(3000);
