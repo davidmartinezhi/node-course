0567275a9bbc84f99361c38c90f010c6fc69767c
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-05-16 11:21:26 -0600

    added rest apis

--- typescript-node/app.ts
+++ typescript-node/app.ts
@@ -1,13 +1,22 @@
 import express from 'express';
-
-const app = express();
+import bodyParser from 'body-parser';
+import todoRoutes from './routes/todos';
 
 /*
 npm install --save-dev @types/node
 npm install --save-dev @types/express
+npm install --save-dev @types/body-parser
 
 we will find many @types packages because they do
 translations from JavaScript to TypeScript, so TypeScript can understand them.
 */
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(todoRoutes);
+
+
 
 app.listen(3000);
